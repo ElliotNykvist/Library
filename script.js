308e128f5f7bbe10d930876ab6f788c6feb7c9b1
Elliot <elliotnykvist@hotmail.se>
2023-08-01 21:50:36 +0200

    Finished the Add book popup

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+  const showModalButton = document.getElementById('add-book');
+  const formModal = document.querySelector('.form');
+  const closeModal = formModal.querySelector('.close-modal');
+
+  showModalButton.addEventListener('click', () => {
+    formModal.classList.add('active'); // Show the form by adding the 'active' class
+  });
+
+  closeModal.addEventListener('click', () => {
+    formModal.classList.remove('active'); // Hide the form by removing the 'active' class
+  });
+
+  // Close the modal if the user clicks outside the modal content
+  window.addEventListener('click', (event) => {
+    if (event.target === formModal) {
+      formModal.classList.remove('active'); // Hide the form by removing the 'active' class
+    }
+  });
+
 
 let myLibrary = [];
 
