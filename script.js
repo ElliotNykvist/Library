25510aaaba76d1c497addef0f97b497525f7ce40
Elliot <elliotnykvist@hotmail.se>
2023-08-02 21:10:49 +0200

    Made a function that push the new book to an array

--- script.js
+++ script.js
@@ -20,10 +20,25 @@
 
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages
+  this.read = read
   // the constructor...
 }
 
 function addBookToLibrary() {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("read").value;
+
+
+  const book = new Book(title, author, pages, read)
+
+  myLibrary.push(book);
+
+  displayLibrary() 
   
 }
