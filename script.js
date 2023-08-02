03b8eac31bd28729415af1835bbd63303af46258
Elliot <elliotnykvist@hotmail.se>
2023-08-02 22:47:36 +0200

    The project is almost finshed every thing works but not perfect

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
   const showModalButton = document.getElementById('add-book');
   const formModal = document.querySelector('.form');
   const closeModal = formModal.querySelector('.close-modal');
+  const submitButton = document.querySelector(".submit")
 
   showModalButton.addEventListener('click', () => {
     formModal.classList.add('active'); // Show the form by adding the 'active' class
@@ -32,7 +33,7 @@ function addBookToLibrary() {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").value;
+  const read = document.getElementById("read").checked;
 
 
   const book = new Book(title, author, pages, read)
@@ -42,3 +43,53 @@ function addBookToLibrary() {
   displayLibrary() 
   
 }
+
+function displayLibrary(){
+  const library = document.querySelector(".library");
+  library.innerHTML = '';
+
+  myLibrary.forEach((book, index) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+
+    const bookTitle = document.createElement("h3");
+    bookTitle.textContent = book.title;
+    bookTitle.classList.add("book-title");
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = `Author: ${book.author}`;
+    bookAuthor.classList.add("book-author");
+
+    const bookPages = document.createElement("p");
+    bookPages.textContent = `Pages: ${book.pages}`;
+    bookPages.classList.add("book-pages");
+
+    const bookRead = document.createElement("p");
+    bookRead.textContent = book.read ? "Read" : "Not Read";
+    bookRead.classList.add("book-read")
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "delete";
+    deleteButton.addEventListener("click", () => {
+      myLibrary.splice(index, 1);
+      displayLibrary();
+
+    });
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+    bookCard.appendChild(deleteButton);
+
+    library.appendChild(bookCard);
+  });
+
+}
+
+submitButton.addEventListener("click", addBookToLibrary);
+displayLibrary();
+
+
